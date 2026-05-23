f9bd2ffeb39595b84ee3b86f8b27c82090499274
Mikio Takeuchi <mtake@jp.ibm.com>
2026-05-24 01:30:33 +0900

    WIP ZRAGRetriever

--- tutorials_mtake/langgraph/zrag_util.py
+++ tutorials_mtake/langgraph/zrag_util.py
@@ -10,20 +10,6 @@ class ZRAGRetriever(BaseRetriever):
     """Retriever for Z-related documents"""
     query_dict: dict[str, list[Document]] = {}
 
-    @classmethod
-    def from_dict(cls, query_dict: dict[str, list[Document]], **kwargs: Any) -> "ZRAGRetriever":
-        zrag_retriever = cls()
-        zrag_retriever.query_dict.update(query_dict)
-        return zrag_retriever
-
-    @classmethod
-    def from_json(cls, input_file: str, **kwargs: Any) -> "ZRAGRetriever":
-        query_dict = convert_json_to_dict(input_file)
-        return cls.from_dict(query_dict, **kwargs)
-
-    def add_dict(self, query_dict: dict[str, list[Document]], **kwargs: Any):
-        self.query_dict.update(query_dict)
-
     def add_query(self, query: str, documents: list[Document], **kwargs: Any):
         self.query_dict[query] = documents
 
@@ -31,10 +17,24 @@ class ZRAGRetriever(BaseRetriever):
     def _get_relevant_documents(self, query: str, *, run_manager: CallbackManagerForRetrieverRun) -> list[Document]:
         return self.query_dict.get(query, [])
 
+    def add_dict(self, query_dict: dict[str, list[Document]], **kwargs: Any):
+        self.query_dict.update(query_dict)
 
-def convert_json_to_dict(input_file: str) -> dict[str, list[Document]]:
+    @classmethod
+    def from_dict(cls, query_dict: dict[str, list[Document]], **kwargs: Any) -> "ZRAGRetriever":
+        zrag_retriever = cls()
+        zrag_retriever.add_dict(query_dict, **kwargs)
+        return zrag_retriever
+
+    @classmethod
+    def from_json(cls, query_file: str, **kwargs: Any) -> "ZRAGRetriever":
+        query_dict = convert_json_to_dict(query_file)
+        return cls.from_dict(query_dict, **kwargs)
+
+
+def convert_json_to_dict(query_file: str) -> dict[str, list[Document]]:
     query_dict: dict[str, list[Document]] = {}
-    with open(input_file, "r", encoding="utf-8") as fin:
+    with open(query_file, "r", encoding="utf-8") as fin:
         for line in fin:
             obj = json.loads(line)
             query = obj["query"]
