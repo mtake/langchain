eb18dd15152c8f0f7b558275e8bc6e1f93265ba4
Mikio Takeuchi <mtake@jp.ibm.com>
2026-05-24 01:17:24 +0900

    WIP ZRAGRetriever

--- tutorials_mtake/langgraph/zrag_util.py
+++ tutorials_mtake/langgraph/zrag_util.py
@@ -1,38 +1,39 @@
+import json
 from typing import override, Any
 
 from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
-
 from langchain_core.retrievers import BaseRetriever
 
 
 class ZRAGRetriever(BaseRetriever):
     """Retriever for Z-related documents"""
-    _documents_dict: dict[str, list[Document]] = {}
+    query_dict: dict[str, list[Document]] = {}
 
     @classmethod
-    def from_dict(cls, documents_dict: dict[str, list[Document]], **kwargs: Any) -> "ZRAGRetriever":
+    def from_dict(cls, query_dict: dict[str, list[Document]], **kwargs: Any) -> "ZRAGRetriever":
         zrag_retriever = cls()
-        zrag_retriever._documents_dict.update(documents_dict)
+        zrag_retriever.query_dict.update(query_dict)
         return zrag_retriever
 
-    def add_dict(self, documents_dict: dict[str, list[Document]], **kwargs: Any):
-        self._documents_dict.update(documents_dict)
+    @classmethod
+    def from_json(cls, input_file: str, **kwargs: Any) -> "ZRAGRetriever":
+        query_dict = convert_json_to_dict(input_file)
+        return cls.from_dict(query_dict, **kwargs)
+
+    def add_dict(self, query_dict: dict[str, list[Document]], **kwargs: Any):
+        self.query_dict.update(query_dict)
 
     def add_query(self, query: str, documents: list[Document], **kwargs: Any):
-        self._documents_dict[query] = documents
+        self.query_dict[query] = documents
 
     @override
     def _get_relevant_documents(self, query: str, *, run_manager: CallbackManagerForRetrieverRun) -> list[Document]:
-        return self._documents_dict.get(query, [])
+        return self.query_dict.get(query, [])
 
-
-import json
 
 def convert_json_to_dict(input_file: str) -> dict[str, list[Document]]:
-
-    documents_dict: dict[str, list[Document]] = {}
-
+    query_dict: dict[str, list[Document]] = {}
     with open(input_file, "r", encoding="utf-8") as fin:
         for line in fin:
             obj = json.loads(line)
@@ -44,6 +45,5 @@ def convert_json_to_dict(input_file: str) -> dict[str, list[Document]]:
                 metadata = document
                 lc_document = Document(page_content=page_content, metadata=metadata)
                 lc_documents.append(lc_document)
-            documents_dict[query] = lc_documents
-
-    return documents_dict
+            query_dict[query] = lc_documents
+    return query_dict
